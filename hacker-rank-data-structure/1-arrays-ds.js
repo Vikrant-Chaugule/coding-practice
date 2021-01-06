88859944806585e6d43216896c87e3f4f0e16b98
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2021-01-06 18:39:22 +0530

    completed `arrays-ds` problem

---
+++ hacker-rank-data-structure/1-arrays-ds.js
@@ -0,0 +1,19 @@
+/* 
+An array is a type of data structure that stores elements of the same type in a contiguous block of memory.
+
+Reverse an array of integers.
+
+Example
+A = [1,2,3]
+Return [3,2,1]
+*/
+
+function reverseArray(a) {
+    const length = a.length;
+    if(length === 1) return a;
+    const resultArray = [];
+    for(let i=length-1;i>=0;i--){
+        resultArray.push(a[i]);
+    }
+    return resultArray;
+}
